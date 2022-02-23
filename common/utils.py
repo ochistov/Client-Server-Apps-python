fdc8de37585108b59c0a01207762520db84eb39a
ochistov <chistov95@gmail.com>
2022-02-23 22:52:46 +0300

    all done

--- common/utils.py
+++ common/utils.py
@@ -2,33 +2,48 @@ import argparse
 import socket
 import json
 import common.variables as v
+import logging
+import inspect
+from functools import wraps
 
 ADDRESS = 'localhost'
 PORT = 7777
 CONNECTIONS = 50
 
+serverLogger = logging.getLogger('server')
+clientLogger = logging.getLogger('client')
 
+def log(func):
+    @wraps(func)
+    def call(*args, **kwargs):
+        outerFunc = inspect.stack()[1][3]
+        serverLogger.debug(f'Function "{func.__name__}" is called into "{outerFunc}"')
+        clientLogger.debug(f'Function "{func.__name__}" is called into "{outerFunc}"')
+        return func(*args, **kwargs)
+    return call
+
+@log
 def getServerSocket(addr, port):
     s = socket.socket()
     s.bind((addr, port))
     s.listen(CONNECTIONS)
     return s
 
-
+@log
 def getClientSocket(addr, port):
     s = socket.socket()
     s.connect((addr, port))
     return s
 
-
+@log
 def sendData(recipient, data):
     recipient.send(json.dumps(data).encode(v.ENCODING))
 
-
+@log
 def getData(sender):
     return json.loads(sender.recv(1024).decode(v.ENCODING))
 
-
+@log
 def createParser():
     parser = argparse.ArgumentParser()
 
